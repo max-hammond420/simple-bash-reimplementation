d07b524268e8fc47c5e74a9af2a6f1fc7ebff12b
Max <max@Maxs-MacBook-Air.local>
2022-09-04 18:44:11 +1000

    added file

---
+++ commands/commands1.py
@@ -0,0 +1,17 @@
+import sys
+
+def exit_cmd(user):
+    print("Bye, <current_user>")
+    sys.exit()
+
+def cd():
+    pass
+
+def pwd():
+    pass
+
+def mkdir():
+    pass
+
+def touch():
+    pass
