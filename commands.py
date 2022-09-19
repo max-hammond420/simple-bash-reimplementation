a78a8b3c38794531f0bab7fb8b7a1af0f6fc5527
endeavour <max@endeavour>
2022-09-19 15:44:51 +1000

    working on rmdir

--- commands.py
+++ commands.py
@@ -335,9 +335,6 @@ def rmdir(args, cwd, root, user):
     child = path[-1].get_child(rm_file)
 
     # check if path is valid
-    if child is None:
-        print("rmdir: Cannot remove pwd")
-        return None
 
     if check_valid_path(path) is False:
         print("rmdir: No such file or directory")
