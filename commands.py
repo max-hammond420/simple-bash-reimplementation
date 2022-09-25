77e0db8f02c4c465c79eacc94c01c47a8cd2c7c3
endeavour <max@endeavour>
2022-09-25 17:41:39 +1000

    working on ls

--- commands.py
+++ commands.py
@@ -128,7 +128,7 @@ def ls(args, cwd, user, root):
     # special case if ls points to a file
     if type(dir) is File:
         if dash_l:
-            return dir.get_permissions() + " " + dir.get_owner() + " " + dir.get_name() "\n"
+            return f"{dir.get_permissions()} {dir.get_owner()} {dir.get_name()}"
         return dir.get_name()+'\n'
 
     items = dir.get_item_names()
