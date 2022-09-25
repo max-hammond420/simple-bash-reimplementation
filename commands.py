c641690558e0bd1b1b34acf2d2299177d6c27475
endeavour <max@endeavour>
2022-09-25 22:22:04 +1000

    working on ls

--- commands.py
+++ commands.py
@@ -177,7 +177,9 @@ def ls(args, cwd, user, root):
             ls.append(f"{items_obj[i].get_permissions()} {items_obj[i].get_owner()} {items_obj[i].get_name()}")
 
         s = '\n'.join(ls)
-        if len(ls) > 2:
+        if (dash_a is True) and (len(ls) > 2):
+            s += '\n'
+        elif (dash_a is False) and (len(ls) > 0):
             s += '\n'
     else:
         items = sorted(items)
